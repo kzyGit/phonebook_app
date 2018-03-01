7cc7f8b62ad080da021edc5b18aef3580e1e6eb5
kzyGit <kezzyangiro@gmail.com>
2018-03-01 17:58:51 +0300

    Update phonebook file

---
+++ app/phonebook.py
@@ -0,0 +1,16 @@
+phonebook = {}
+
+def add_contact(name, number):
+    return {"message": "Contact Added successfully"} 
+
+
+def update_contact(contact1, contact2): 
+    return {"message": "Contact updated successfully"}
+
+
+def delete_contact(name, number):
+    return {"message": "Contact deleted"}
+
+
+def view_contact(name, number):
+    return {"message": "Contact found"}
